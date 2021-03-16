75fce4f559bef5c61457d9c5b3c2fca42fa80cd8
Ding <dingwindivid@gmail.com>
2021-03-16 22:13:13 +0900

    test

--- src/com/awarematics/postmedia/algorithms/similarity/MHausFOV.java
+++ src/com/awarematics/postmedia/algorithms/similarity/MHausFOV.java
@@ -106,7 +106,8 @@ public class MHausFOV implements MSimilarityMeasure  {
 				}
 			}
 			minDistances2.add(min);
-		}
+		}     
+		
 
 		MGeometryFactory geometryFactory = new MGeometryFactory();
 		double[] tempList = minDistances1.stream().mapToDouble(i -> i).toArray();
